23bf5cf1e49a63e80e2be788ab27c03984e5d59f
Alexandre De Carli <alexandre.decarli@softfocus.com.br>
2020-11-28 12:04:01 -0300

    Adicionado gateway

--- produtos/src/main/kotlin/br/com/softfocus/produtos/api/ProdutoApi.kt
+++ produtos/src/main/kotlin/br/com/softfocus/produtos/api/ProdutoApi.kt
@@ -2,11 +2,10 @@ package br.com.softfocus.produtos.api
 
 import br.com.softfocus.produtos.model.Produto
 import br.com.softfocus.produtos.repository.ProdutoRepository
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.jms.core.JmsTemplate
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 
@@ -16,6 +15,11 @@ class ProdutoApi {
     @Autowired
     private lateinit var repository: ProdutoRepository
 
+    @Autowired
+    private lateinit var jmsTemplate: JmsTemplate
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
     @GetMapping
     fun all(): MutableList<Produto?> {
         return repository.findAll()
@@ -24,5 +28,20 @@ class ProdutoApi {
     @GetMapping("{id}")
     fun one(@PathVariable("id") id: Long): Optional<Produto?> {
         return repository.findById(id)
+    }
+
+    @PostMapping
+    fun insert(@RequestBody produto: Produto): Produto? {
+
+        var produtoJson = objectMapper.writeValueAsString(produto)
+
+        jmsTemplate.convertAndSend("queue.produto.insert", produtoJson)
+
+        return repository.save(produto)
+    }
+
+    @DeleteMapping("{id}")
+    fun delete(@PathVariable("id") id: Long?) {
+        repository.deleteById(id!!)
     }
 }
