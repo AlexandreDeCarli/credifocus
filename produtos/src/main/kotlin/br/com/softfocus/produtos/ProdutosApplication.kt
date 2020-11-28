d1b7f910e027833166f10e9442fd5a70fd6eb4c6
Alexandre De Carli <alexandre.decarli@softfocus.com.br>
2020-11-28 11:09:26 -0300

    Adicionado serviço Produtos

--- produtos/src/main/kotlin/br/com/softfocus/produtos/ProdutosApplication.kt
+++ produtos/src/main/kotlin/br/com/softfocus/produtos/ProdutosApplication.kt
@@ -2,8 +2,10 @@ package br.com.softfocus.produtos
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class ProdutosApplication
 
 fun main(args: Array<String>) {
