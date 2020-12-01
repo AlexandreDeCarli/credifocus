9f4f250a1157c521d835e40cc0bcfa410aabad38
Alexandre De Carli <alexandre.decarli@softfocus.com.br>
2020-12-01 18:41:25 -0300

    Adicionado serviço de cupons

--- vendas/src/main/kotlin/br/com/softfocus/vendas/VendasApplication.kt
+++ vendas/src/main/kotlin/br/com/softfocus/vendas/VendasApplication.kt
@@ -2,10 +2,12 @@ package br.com.softfocus.vendas
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 class VendasApplication
 
 fun main(args: Array<String>) {
